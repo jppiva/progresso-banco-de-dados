eb101d0bce2b89134a098825e1b6d5fbe9fa1113
joao pedro piva <joaopedropiva17@gmail.com>
2025-09-15 15:58:59 -0300

    cria as queries de select

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -1,5 +1,5 @@
-CREATE TEBLE artista(
-    id SERIAL PRIMARY KEY ,
+CREATE TABLE artista(
+    id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     tipo VARCHAR(20) NOT NULL,
     membros INTEGER,
@@ -8,5 +8,19 @@ CREATE TEBLE artista(
     pais_origem VARCHAR(50),
     musica_famosa VARCHAR(50),
     ativo BOOLEAN
-
 );
+
+INSERT INTO artista(nome, tipo, membros, genero_musical, ano_lancamento, pais_origem, musica_famosa, ativo)
+VALUES
+('Adele', 'solo', 1, 'pop', 2006, 'Reino Unido', 'Rolling in the Deep', TRUE),
+('Twenty One Pilots', 'banda', 2, 'rock alternativo', 2009, 'Estados Unidos', 'Stressed Out', TRUE),
+('Coldplay', 'banda', 4, 'rock alternativo', 1996, 'Reino Unido', 'Viva la Vida', TRUE),
+('The Beatles', 'banda', 4, 'rock', 1960, 'Reino Unido', 'Hey Jude', FALSE),
+('Shakira', 'solo', 1, 'pop', 1990, 'Colômbia', 'Hips Don’t Lie', TRUE),
+('AC/DC', 'banda', 5, 'rock', 1973, 'Austrália', 'Back in Black', TRUE),
+('Anitta', 'solo', 1, 'pop/funk', 2010, 'Brasil', 'Show das Poderosas', TRUE),
+('Imagine Dragons', 'banda', 4, 'pop rock', 2008, 'Estados Unidos', 'Believer', TRUE),
+('BTS', 'banda', 7, 'k-pop', 2013, 'Coreia do Sul', 'Dynamite', TRUE),
+('Metallica', 'banda', 4, 'heavy metal', 1981, 'Estados Unidos', 'Enter Sandman', TRUE),
+('Taylor Swift', 'solo', 1, 'pop/country', 2006, 'Estados Unidos', 'Love Story', TRUE),
+('Queen', 'banda', 4, 'rock', 1970, 'Reino Unido', 'Bohemian Rhapsody', FALSE);
---
+++ queries/02-selects.sql
@@ -0,0 +1,8 @@
+SELECT * FROM artista;
+
+SELECT COUNT (*) AS total_artistas FROM artista
+
+SELECT nome, genero_musical FROM artista;
+
+SELECT nome, genero_musical from artista
+WHERE id = 9 AND id = 3;
